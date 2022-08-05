3c3c86ceacaf81c29c69a30a3ecd6981b22cb0f3
Alberto <67739785+albycrescini@users.noreply.github.com>
2022-08-05 21:15:33 +0200

    Working on fetching data from the website

--- backend-dev/scraper.py
+++ backend-dev/scraper.py
@@ -1,15 +1,22 @@
 from seleniumwire import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 options = webdriver.ChromeOptions()
-options.headless = True
+# options.headless = True
 
 CHROME_DRIVER_PATH = "/Users/albertocrescini/Downloads/chromedriver"
 URL = "https://hackathon.bz.it"
 
 driver = webdriver.Chrome(options=options, executable_path=CHROME_DRIVER_PATH)
+
 driver.get(URL)
+
+# IGNORE COOKIES:
+driver.find_element(by=By.XPATH, value='''//*[@id="pc-button"]/button''').click()
 
 def text_research(input: str, xpath=None):
     try:
@@ -18,9 +25,25 @@ def text_research(input: str, xpath=None):
             return input == element.get_attribute('innerText')
         else:
             return input in driver.page_source
+        
     except NoSuchElementException:
-        print("Unable to find xpath: %s"%xpath)
+        return "Unable to find xpath: %s"%xpath
 
-#coming-soon > div > div > div > div > a
-#print(text_research("NOI Hackathon Summer Edition"))
+def wait_element_load(xpath: str):
+    try:
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    except TimeoutException:
+        return "Request timeout."
+    return element
+
+def button_click(xpath: str):
+    try:
+        element = wait_element_load(xpath)
+        element.click()
+    except ElementClickInterceptedException:
+        return "element click intercepted: Other element would receive the click."
+
+print(text_research("NOI Hackathon Summer Edition"))
 print(text_research("Read more", '''//*[@id="coming-soon"]/div/div/div/div/a'''))
+print(wait_element_load('''//*[@id="coming-soon"]/div/div/div/div/a'''))
+print(button_click('''/html/body/footer/div[2]/div[5]/p/a'''))
