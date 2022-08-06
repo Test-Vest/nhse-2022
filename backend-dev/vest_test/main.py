17cf1845d6d24bfef01f31199f80801c2edbd65f
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-08-06 02:07:51 +0200

    feat: wsgi and making prod ready

--- backend-dev/vest_test/main.py
+++ backend-dev/vest_test/main.py
@@ -1,12 +1,15 @@
 import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
