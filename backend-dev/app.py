d7d8f7d7fdce4f0776ea1600f626c28808017dd2
Alberto <67739785+albycrescini@users.noreply.github.com>
2022-08-06 06:57:52 +0200

    Working on fetching data from the website

--- backend-dev/app.py
+++ backend-dev/app.py
@@ -1,12 +1,41 @@
+from crypt import methods
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+from pywebcopy import save_website
 
-app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+WEBSITE = "hackathon.bz.it"
+URL = "https://" + WEBSITE
+
+app = Flask(
+    __name__,
+    static_url_path='', 
+    static_folder='websites/hackathon_site/' + WEBSITE)
+
+def download_website():
+    print("Started download of website: %s" %WEBSITE)
+    FILENAME = "hackathon_site"
+    FOLDER = APP_ROOT + "/websites"
+    
+    return save_website(
+        url=URL,
+        project_folder=FOLDER,
+        project_name=FILENAME,
+        bypass_robots=True,
+        debug=True,
+        open_in_browser=True,
+        delay=None,
+        threaded=True,
+    )
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/download', methods=['GET'])
+def download():
+    return send_from_directory("websites/hackathon_site/" + WEBSITE, "index.html")
+
 if __name__ == "__main__":
+    download_website()
     app.run(debug=True)
--- backend-dev/scraper.py
+++ backend-dev/scraper.py
@@ -1,27 +1,32 @@
+from xml.etree.ElementPath import xpath_tokenizer
 from seleniumwire import webdriver
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from pywebcopy import save_website
 
 options = webdriver.ChromeOptions()
 # options.headless = True
 
 CHROME_DRIVER_PATH = "/Users/albertocrescini/Downloads/chromedriver"
-URL = "https://hackathon.bz.it"
+#URL = "https://hackathon.bz.it"
+URL = "https://hackathon.bz.it/secure/login"
 
 driver = webdriver.Chrome(options=options, executable_path=CHROME_DRIVER_PATH)
 
 driver.get(URL)
 
-# IGNORE COOKIES:
-driver.find_element(by=By.XPATH, value='''//*[@id="pc-button"]/button''').click()
+def accept_cookies():
+    try:
+        driver.find_element(by=By.XPATH, value='''//*[@id="pc-button"]/button''').click()
+    except NoSuchElementException:
+        pass
 
 def text_research(input: str, xpath=None):
     try:
         if xpath:
-            element = driver.find_element(by=By.XPATH, value=xpath)
+            element = wait_element_load(xpath)
             return input == element.get_attribute('innerText')
         else:
             return input in driver.page_source
@@ -41,9 +46,36 @@ def button_click(xpath: str):
         element = wait_element_load(xpath)
         element.click()
     except ElementClickInterceptedException:
-        return "element click intercepted: Other element would receive the click."
+        return "Element click intercepted: other element would receive the click."
 
+def set_field(args: dict, submit_xpath: str):
+    # mi aspetto {"key": "value"}
+    for xpath, value in args.items():
+        element = wait_element_load(xpath)
+        element.send_keys(value)
+    wait_element_load('//*[@id="_username"]').submit()
+
+def download_website():
+    FILENAME = "hackathon_site"
+    FOLDER = "/Users/albertocrescini/Desktop/nhse-2022/backend-dev/template"
+    
+    return save_website(
+        url=URL,
+        project_folder=FOLDER,
+        project_name=FILENAME,
+        bypass_robots=True,
+        debug=True,
+        open_in_browser=True,
+        delay=None,
+        threaded=True,
+    )
+    
+accept_cookies()
 print(text_research("NOI Hackathon Summer Edition"))
 print(text_research("Read more", '''//*[@id="coming-soon"]/div/div/div/div/a'''))
 print(wait_element_load('''//*[@id="coming-soon"]/div/div/div/div/a'''))
 print(button_click('''/html/body/footer/div[2]/div[5]/p/a'''))
+set_field({
+    '//*[@id="_username"]': 'value1',
+    '//*[@id="_password"]': 'value2'
+}, submit_xpath='//*[@id="_submit"]')
