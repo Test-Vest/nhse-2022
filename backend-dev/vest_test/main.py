ed50f9c200f1b802c89775696bdb6ab95d09555b
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-08-06 02:37:37 +0200

    fix: switch to gunicorn from waitress

--- backend-dev/vest_test/main.py
+++ backend-dev/vest_test/main.py
@@ -9,3 +9,7 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
--- backend-dev/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from vest_test.main import app
-from waitress import serve
-
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=os.environ.get("PORT") or 8080)
